259226950091f4d8f71a44daf673341a6a6ed1b9
sti433 <sti.ri433@gmail.com>
2025-11-06 20:31:18 +0330

    Add files via upload

---
+++ Program(1).cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication125
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[10];
+            float sum = 0, avg;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine("enter adad");
+                arr[i] = int.Parse(Console.ReadLine());
+                sum = sum + arr[i];
+            }
+            avg = sum / 10;
+            Console.WriteLine("sum=" + sum);
+            Console.WriteLine("avg=" + avg);
+            Console.ReadKey();
+        }
+    }
+}
